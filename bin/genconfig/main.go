8efa7a4ed2fed6a29a6050fe95039d60c4b0fd78
Eugene Platonov <jozic@live.com>
2015-08-05 16:28:49 -0400

    read config from etcd-cluster

--- bin/genconfig/main.go
+++ bin/genconfig/main.go
@@ -10,6 +10,10 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"github.com/coreos/go-etcd/etcd"
+	"net"
+	"encoding/json"
+	"time"
 )
 
 // first 3 args are configDir,
@@ -53,6 +57,30 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+	} else if strings.ToLower(mode) == "etcd-cluster" {
+		// configDir dataDir etcd-cluster group numHosts [etcdUrl]
+		group := args[3]
+		numHosts, err := strconv.Atoi(args[4])
+		if err != nil {
+			panic(err)
+		}
+		etcdUrl := "http://localhost:2379"
+		if len(args) > 5 {
+			etcdUrl = args[5]
+		}
+		hosts, err := readEtcdConfig(etcdUrl, group, numHosts)
+		if err != nil {
+			panic(err)
+		}
+		dataDirs := make([]string, len(hosts), len(hosts))
+		for i, _ := range hosts {
+			dataDirs[i] = dataDir
+		}
+		cfgs := config.AutoCluster(100, hosts, dataDirs)
+		err = writeConfigs(cfgs, configDir)
+		if err != nil {
+			panic(err)
+		}
 	} else {
 		usage(args)
 		return
@@ -61,12 +89,12 @@ func main() {
 }
 
 func usage(args []string) {
-	log.Printf("First arg should either be 'singlenode' or 'cluster'.  Args provided : %+v", args)
+	log.Printf("First arg should either be 'singlenode' or 'cluster or 'etcd-cluster'.  Args provided : %+v", args)
 	log.Printf(`Usage: \
-		first arg is MODE, either "singlenode" or "cluster" \
+		first arg is MODE, either "singlenode" or "cluster" or "etcd-cluster"\
 		if singlenode, 2nd arg is output directory, 3rd arg is number of shards.  we'll generate 3 datacenters "dc1,dc2,dc3" and a node in each for each shard \
-		if cluster, 2nd arg is output directory, 3rd arg is a TSV file denoting datacenter,host.`)
-
+		if cluster, 2nd arg is output directory, 3rd arg is a TSV file denoting datacenter,host\
+		if etcd-cluster, 2nd arg is output directory, 3rd arg is an etcd url (used to read cluster configuration).`)
 }
 
 func writeConfigs(cfgs []config.ClusterConfig, configDir string) error {
@@ -81,6 +109,11 @@ func writeConfigs(cfgs []config.ClusterConfig, configDir string) error {
 			return err
 		}
 	}
+	m, err := json.Marshal(cfgs)
+	if (err != nil){
+		panic(err)
+	}
+	log.Println("config: %s", string(m))
 	return nil
 }
 
@@ -119,4 +152,147 @@ func readHosts(hostPath string) (hosts []config.Host, err error) {
 		}
 	}
 	return ret, nil
+}
+
+func readEtcdConfig(etcdUrl string, group string, numHosts int) (hosts []config.Host, err error) {
+	etcdClient := etcd.NewClient([]string{etcdUrl})
+	namespacePrefix := "/raftis/cluster/"
+	ip := myIp()
+	if ip == ""{
+		panic("ip can't be empty, something went wrong")
+	}
+	log.Printf("local ip resoled to " + ip)
+	amIAMaster, startIndex, err := tryBecomeBootstrapMaster(etcdClient, namespacePrefix, ip)
+	log.Printf("Am i a Master? %t",amIAMaster)
+	if err != nil {
+		panic(err)
+	}
+	nodesKey := namespacePrefix + "nodes"
+	configKey := namespacePrefix + "hostsConfig"
+	err = registerMyself(etcdClient, nodesKey, ip, group)
+	if (err != nil) {
+		panic(err)
+	}
+	log.Printf("Registered myself!")
+	if amIAMaster {
+		err = waitForAllToRegister(etcdClient, nodesKey, numHosts, startIndex)
+		if (err != nil) {
+			panic(err)
+		}
+		hosts, err := buildHostsConfig(etcdClient, nodesKey)
+		if (err != nil) {
+			panic(err)
+		}
+		 err = publishHostsConfig(etcdClient, configKey, hosts)
+		if (err != nil) {
+			panic(err)
+		}
+		return hosts, nil
+	} else {
+		log.Printf("Waiting for Master to publish config...")
+		return readHostsConfig(etcdClient, configKey)
+	}
+}
+
+//tries to create /raftis/cluster/bootstrapMaster key and
+// returnes amIAMaster = true if suceeds, false otherwise
+func tryBecomeBootstrapMaster(etcdClient *etcd.Client,
+								namespacePrefix string,
+								ip string) (amIAMaster bool, currentIndex uint64, err error) {
+	resp, err := etcdClient.Create(namespacePrefix + "bootstrapMaster", ip, 0)
+	if err != nil {
+		v, ok := err.(*etcd.EtcdError)
+		if ok && v.Message == "Key already exists" {
+			return false, 0, nil
+		} else {
+			return false, 0, err
+		}
+	}
+	return true, resp.Node.ModifiedIndex, nil
+}
+
+// registers local ip and `group` under `nodesKey`
+// used by bootstrap `master` and `followers`
+func registerMyself(etcdClient *etcd.Client, nodesKey string, ip string, group string) error {
+	_, err := etcdClient.Create(fmt.Sprintf("%s/%s", nodesKey, ip + "::" + group), time.Now().String(), 0)
+	return err
+}
+
+//recursive function waiting for `left` hosts to register themselves under `nodesKey` key.
+func waitFor(etcdClient *etcd.Client, nodesKey string, left int, index uint64) error {
+	if left < 1 {
+		return nil
+	}
+	log.Printf("watinig for %d more node(s) to join on index %d...", left, index)
+	resp, err := etcdClient.Watch(nodesKey, index, true, nil, nil)
+	if err != nil {
+		return err
+	}
+	lastNext := resp.Node.ModifiedIndex + 1
+	return waitFor(etcdClient, nodesKey, left - 1, lastNext)
+}
+
+//wait for `numHosts` hosts to be registered under `nodesKey`. Start waiting since `startIndex`
+// used by bootstrap master
+func waitForAllToRegister(etcdClient *etcd.Client, nodesKey string, numHosts int, startIndex uint64) error {
+	return waitFor(etcdClient, nodesKey, numHosts, startIndex)
+}
+
+//reads `nodesKey` after all nodes in cluster are registered under it, builds and returns Hosts
+// used by bootstrap `master`
+func buildHostsConfig(etcdClient *etcd.Client, nodesKey string) ([]config.Host, error) {
+	resp, err := etcdClient.Get(nodesKey, false, true)
+	if err != nil {
+		return nil, err
+	}
+	hosts := make([]config.Host, 0, 0)
+	for _, node := range resp.Node.Nodes {
+		hostGroup := strings.Split(strings.TrimPrefix(node.Key, nodesKey + "/"), "::")
+		host := hostGroup[0]
+		group := hostGroup[1]
+		//todo remove duplication
+		h := config.Host{
+			RedisAddr:    fmt.Sprintf("%s:%d", host, 8679),
+			FlotillaAddr: fmt.Sprintf("%s:%d", host, 1103),
+			Group:        group,
+		}
+		hosts = append(hosts, h)
+	}
+	return hosts, nil
+}
+
+// marshals given hosts into json and sets it as a value for config key.
+// used by bootstrap `master`
+func publishHostsConfig(etcdClient *etcd.Client, configKey string, hosts []config.Host) error {
+	marshaled, err := json.Marshal(hosts)
+	if err != nil {
+		return err
+	}
+	_, err = etcdClient.Create(configKey, string(marshaled), 0)
+	return err
+}
+
+// waits for config to be published under `configKey` and once it's published reads, unmarshals and returns Hosts.
+// used by bootstrap `follower`
+func readHostsConfig(etcdClient *etcd.Client, configKey string) (hosts []config.Host, err error) {
+	resp, err := etcdClient.Watch(configKey, 0, false, nil, nil)
+	if (err != nil) {
+		return nil, err
+	}
+	err = json.Unmarshal([]byte(resp.Node.Value), &hosts)
+	return hosts, err
+}
+
+//returns "" empty string if ip can't be obtained, which should never happen
+func myIp() string {
+	addrs, err := net.InterfaceAddrs()
+	if (err != nil){
+		panic(err)
+	}
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil  {
+			return ipnet.IP.String()
+		}
+	}
+	return ""
 }
